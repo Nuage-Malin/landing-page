ceba3f458db3797264312747d7c4e8409e291da5
Zarfir <zarfir.29@gmail.com>
2022-02-04 12:27:41 +0100

    [+] WhoAreWe.svelte without bootstrape and responsive

--- src/WhoAreWe.svelte
+++ src/WhoAreWe.svelte
@@ -1,65 +1,101 @@
 <script>
-    import { Container, Row, Col } from "sveltestrap";
 </script>
 
-<Container>
-    <div class="mt-7" />
-    <Row>
-        <Col sm="12" md={{ size: 6, offset: 3 }}>
-            <div class="fs-1 title">Qui sommes-nous ?</div>
-            <div class="mt-7" />
-            <div class="fs-5 paragraphe">
-                Nous sommes 9 étudiants à Epitech Rennes et nous développons ce
-                projet dans le cadre de nos études.
-            </div>
-            <div class="fs-5 paragraphe">
-                Notre mission est l'ultra optimisation pour que Nuage Malin
-                puisse être le service le plus écologique possible.
-            </div>
-            <div class="mt-3" />
-            <div class="fs-5 paragraphe">
-                Nous développons activement une solution cloud écologique qui
-                permettra d'archiver vos données tout en pensant à demain.
-            </div>
-            <div class="mt-15" />
-            <div class="fs-1 title">Pourquoi nous faire confiance ?</div>
-            <div class="mt-7" />
-            <div class="fs-5 paragraphe">
-                Notre équipe s'investit énormément jour après jour pour garder
-                en ligne de mire leurs valeurs, celles qui leurs ont donné envie
-                de créer ce projet : la réduction de notre consommation
-                énergétique.
-            </div>
-            <div class="mt-3" />
-            <div class="fs-5 paragraphe">
-                Nuage Malin est un projet entièrement pensé et réfléchi en
-                France, plus particulièrement à Rennes. De sa position
-                géographique jusqu'à son nom, Nuage Malin s'engage à rester
-                Français.
-            </div>
-        </Col>
-    </Row>
-</Container>
+<div class="center">
+    <div class="mt-xl title">Qui sommes-nous ?</div>
+    <div class="mt-l paragraphe">
+        Nous sommes 9 étudiants à Epitech Rennes et nous développons ce
+        projet dans le cadre de nos études.
+    </div>
+    <div class="paragraphe">
+        Notre mission est l'ultra optimisation pour que Nuage Malin
+        puisse être le service le plus écologique possible.
+    </div>
+    <div class="mt-l paragraphe">
+        Nous développons activement une solution cloud écologique qui
+        permettra d'archiver vos données tout en pensant à demain.
+    </div>
+    <div class="mt-xl title">Pourquoi nous faire confiance ?</div>
+    <div class="mt-l paragraphe">
+        Notre équipe s'investit énormément jour après jour pour garder
+        en ligne de mire leurs valeurs, celles qui leurs ont donné envie
+        de créer ce projet : la réduction de notre consommation
+        énergétique.
+    </div>
+    <div class="mt-l paragraphe">
+        Nuage Malin est un projet entièrement pensé et réfléchi en
+        France, plus particulièrement à Rennes. De sa position
+        géographique jusqu'à son nom, Nuage Malin s'engage à rester
+        Français.
+    </div>
+</div>
 
 <style>
+    :root {
+        --fs-xl: clamp(1.5rem, 1vw + 1.5rem, 2.5rem);
+        --fs-s: clamp(0.8rem, 1vw + 0.6rem, 1.2rem);
+        --ml-mr: 32%;
+        --mt-xl: 15%;
+        --mt-l: 7%;
+    }
+    .center {
+        margin-left: var(--ml-mr);
+        margin-right: var(--ml-mr);
+    }
     .title {
         text-align: center;
         font-family: "Quicksand", sans-serif;
         color: #4881d5;
-        margin: auto;
+        font-size: var(--fs-xl);
     }
     .paragraphe {
         font-family: "Hammersmith One", sans-serif;
         color: #858585;
-        margin: auto;
+        font-size: var(--fs-s);
     }
-    .mt-15 {
-        margin-top: 15%;
+    .mt-xl {
+        margin-top: var(--mt-xl);
     }
-    .mt-7 {
-        margin-top: 7%;
+    .mt-l {
+        margin-top: var(--mt-l);
     }
-    .mt-3 {
-        margin-top: 3.5%;
+    @media (max-width: 1640px) {
+        :root {
+            --ml-mr: 27%;
+        }
     }
+    @media (max-width: 1000px) {
+        :root {
+            --ml-mr: 22%;
+            --mt-xl: 15%;
+            --mt-l: 10%;
+        }
+    }
+    @media (max-width: 800px) {
+        :root {
+            --ml-mr: 17%;
+            --mt-xl: 20%;
+            --mt-l: 12%;
+        }
+    }
+    @media (max-width: 600px) {
+        :root {
+            --ml-mr: 15%;
+            --mt-xl: 20%;
+            --mt-l: 15%;
+        }
+    } 
+    @media (max-width: 500px) {
+        :root {
+            --mt-xl: 25%;
+            --mt-l: 15%;
+        }
+    }
+    @media (max-width: 400px) {
+        :root {
+            --mt-xl: 30%;
+            --mt-l: 15%;
+        }
+    }  
+
 </style>
