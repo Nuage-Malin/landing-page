0e67e4c49c9ccb5ddc4e2c1accea04e2561184ae
Zarfir <zarfir.29@gmail.com>
2022-02-20 20:29:56 +0100

    [fix] delete Bootstrape and add responsive

--- src/WhoAreWe.svelte
+++ src/WhoAreWe.svelte
@@ -1,101 +1,58 @@
 <script>
 </script>
 
-<div class="center">
-    <div class="mt-xl title">Qui sommes-nous ?</div>
-    <div class="mt-l paragraphe">
-        Nous sommes 9 étudiants à Epitech Rennes et nous développons ce
-        projet dans le cadre de nos études.
+<div class="container flex">
+    <div>
+        <h1>Qui sommes-nous ?</h1>
+        <p>
+            Nous sommes 9 étudiants à Epitech Rennes et nous développons ce
+            projet dans le cadre de nos études.
+            Notre mission est l'ultra optimisation pour que Nuage Malin
+            puisse être le service le plus écologique possible.
+        </p>
+        <p>
+            Nous développons activement une solution cloud écologique qui
+            permettra d'archiver vos données tout en pensant à demain.            
+        </p>
     </div>
-    <div class="paragraphe">
-        Notre mission est l'ultra optimisation pour que Nuage Malin
-        puisse être le service le plus écologique possible.
-    </div>
-    <div class="mt-l paragraphe">
-        Nous développons activement une solution cloud écologique qui
-        permettra d'archiver vos données tout en pensant à demain.
-    </div>
-    <div class="mt-xl title">Pourquoi nous faire confiance ?</div>
-    <div class="mt-l paragraphe">
-        Notre équipe s'investit énormément jour après jour pour garder
-        en ligne de mire leurs valeurs, celles qui leurs ont donné envie
-        de créer ce projet : la réduction de notre consommation
-        énergétique.
-    </div>
-    <div class="mt-l paragraphe">
-        Nuage Malin est un projet entièrement pensé et réfléchi en
-        France, plus particulièrement à Rennes. De sa position
-        géographique jusqu'à son nom, Nuage Malin s'engage à rester
-        Français.
+    <div>
+        <h1>Pourquoi nous faire confiance ?</h1>
+        <p>
+            Notre équipe s'investit énormément jour après jour pour garder
+            en ligne de mire leurs valeurs, celles qui leurs ont donné envie
+            de créer ce projet : la réduction de notre consommation
+            énergétique.
+        </p>
+        <p>
+            Nuage Malin est un projet entièrement pensé et réfléchi en
+            France, plus particulièrement à Rennes. De sa position
+            géographique jusqu'à son nom, Nuage Malin s'engage à rester
+            Français.
+        </p>
     </div>
 </div>
 
 <style>
-    :root {
-        --fs-xl: clamp(1.5rem, 1vw + 1.5rem, 2.5rem);
-        --fs-s: clamp(0.8rem, 1vw + 0.6rem, 1.2rem);
-        --ml-mr: 32%;
-        --mt-xl: 15%;
-        --mt-l: 7%;
+    .flex {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
     }
-    .center {
-        margin-left: var(--ml-mr);
-        margin-right: var(--ml-mr);
+    .container {
+        max-width: 40rem;
+        margin-inline: auto;
+        padding-inline: 3rem;
     }
-    .title {
+    h1 {
         text-align: center;
-        font-family: "Quicksand", sans-serif;
-        color: #4881d5;
+        color: var(--c-blue);
+        font-family: var(--ff-t);
+        font-weight: normal;
         font-size: var(--fs-xl);
     }
-    .paragraphe {
-        font-family: "Hammersmith One", sans-serif;
+    p {
+        font-family: var(--ff-p);
         color: #858585;
         font-size: var(--fs-s);
     }
-    .mt-xl {
-        margin-top: var(--mt-xl);
-    }
-    .mt-l {
-        margin-top: var(--mt-l);
-    }
-    @media (max-width: 1640px) {
-        :root {
-            --ml-mr: 27%;
-        }
-    }
-    @media (max-width: 1000px) {
-        :root {
-            --ml-mr: 22%;
-            --mt-xl: 15%;
-            --mt-l: 10%;
-        }
-    }
-    @media (max-width: 800px) {
-        :root {
-            --ml-mr: 17%;
-            --mt-xl: 20%;
-            --mt-l: 12%;
-        }
-    }
-    @media (max-width: 600px) {
-        :root {
-            --ml-mr: 15%;
-            --mt-xl: 20%;
-            --mt-l: 15%;
-        }
-    } 
-    @media (max-width: 500px) {
-        :root {
-            --mt-xl: 25%;
-            --mt-l: 15%;
-        }
-    }
-    @media (max-width: 400px) {
-        :root {
-            --mt-xl: 30%;
-            --mt-l: 15%;
-        }
-    }  
-
 </style>
