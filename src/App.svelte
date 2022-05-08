b1c6e006f3f328b00247b22631f7538562909850
flo-tech <florian.vanier@epitech.eu>
2022-05-08 19:43:12 +0200

    fix(develop): update position of the destination of the NavBar buttons

--- src/App.svelte
+++ src/App.svelte
@@ -22,9 +22,9 @@
     </style>
 </svelte:head>
 
-<div id="whoAreUs" style="position: relative; top: 400px;"/>
-<div id="solution" style="position: relative; top: 1175px;"/>
-<div id="team" style="position: relative; top: 1750px;"/>
+<div id="whoAreUs" style="position: relative; top: 320px;"/>
+<div id="solution" style="position: relative; top: 950px;"/>
+<div id="team" style="position: relative; top: 1280px;"/>
 <div id="contact" style="position: relative; top: 2500px;"/>
 <Router {url}>
     <Route path="*">
